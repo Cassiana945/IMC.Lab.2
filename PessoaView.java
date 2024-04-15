713865743abb0400fea9c8c1fc7d293dce3956d1
Cassiana <160614428+Cassiana945@users.noreply.github.com>
2024-04-15 15:07:44 -0300

    Update PessoaView.java

--- PessoaView.java
+++ PessoaView.java
@@ -1,10 +1,14 @@
 package view;
-
 import br.Pessoa;
-import principal.Principal;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class PessoaView  extends JFrame {
@@ -27,27 +31,38 @@ public class PessoaView  extends JFrame {
     private JTextField textPeso;
     private JLabel lbIMC;
     private JLabel lbSaude;
+    private JLabel lbCPF;
+    private JTextField textCpf;
+    private JButton btnSalvar;
+    private JButton btnAdicionar;
+    private JButton btnLimpar;
     private JPanel pnTitulo;
     private JFrame frame;
     private JLabel jlbTitulo;
     private JButton buttonCalcularIMC;
     private Pessoa pessoa;
+    private List<Pessoa> listaPessoa;
+    private String arquivo;
+
+
+
 
     public PessoaView() { //CONSTRUTOR
 
+        listaPessoa = new ArrayList<>();
         eventosPessoaView();
-        this.pessoa = new Pessoa();
+        arquivo = "C:\\Users\\User\\IdeaProjects\\IMC.Lab.2\\src\\arquivo";
     }
 
-    public void eventosPessoaView() {  //Método botão fechar e clacular IMC
+    public void eventosPessoaView() {
 
-        btnFechar.addActionListener(new ActionListener() {
+        btnFechar.addActionListener(new ActionListener() {   //Método de fechar a tela
             @Override
             public void actionPerformed(ActionEvent e) { System.exit(0); }
 
         });
 
-        btnIMC.addActionListener(new ActionListener() {
+        btnIMC.addActionListener(new ActionListener() {  //Método de ler os atributos e calcular o IMC
             @Override
             public void actionPerformed(ActionEvent e) {
 
@@ -55,6 +70,7 @@ public class PessoaView  extends JFrame {
                 pessoa.setIdade(Integer.parseInt(textIdade.getText()));
                 pessoa.setAltura(Double.parseDouble(textAltura.getText()));
                 pessoa.setPeso(Double.parseDouble(textPeso.getText()));
+                pessoa.setCpf(Integer.parseInt(textCpf.getText()));
                 pessoa.calcularIMC();
                 lbIMC.setText((String.format("%.2f", pessoa.getImc())));
                 lbSaude.setText(pessoa.estadoSaude());
@@ -62,9 +78,62 @@ public class PessoaView  extends JFrame {
                 System.out.println(pessoa.getIdade());
                 System.out.println(pessoa.getAltura());
                 System.out.println(pessoa.getPeso());
+                System.out.println(pessoa.getCpf());
+                System.out.println(pessoa.getImc());
+                System.out.print(pessoa.getEstadoSaude());
             }
         });
+
+        btnLimpar.addActionListener(new ActionListener() {  //Método de limpar tudo
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                lbIMC.setText("   ");
+                lbSaude.setText("  ");
+            }
+        });
+
+        this.pessoa = new Pessoa();
+        btnAdicionar.addActionListener(new ActionListener() {  //Método adicionar os atributos da pessoa
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                Pessoa novaPessoa = new Pessoa();
+                novaPessoa.setNome(textNome.getText());
+                novaPessoa.setIdade(Integer.parseInt(textIdade.getText()));
+                novaPessoa.setAltura(Float.parseFloat(textAltura.getText()));
+                novaPessoa.setPeso(Double.parseDouble(textPeso.getText()));
+                novaPessoa.setCpf(Integer.parseInt(textCpf.getText()));
+                novaPessoa.estadoSaude();
+                listaPessoa.add(novaPessoa);
+                textNome.setText("");
+                textIdade.setText("");
+                textAltura.setText("");
+                textPeso.setText("");
+            }
+        });
+
+        btnSalvar.addActionListener(new ActionListener() {  //Método de salvar os atributos da pessoa no arquivo.txt
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                salvarNoArquivo("arquivo");
+            }
+        });
+
     }
+
+    private void salvarNoArquivo(String listapessoas) {  //Método de ler e escrever os atributos salvos no arquivo.txt
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(arquivo))) {
+            for (Pessoa pessoa : listaPessoa) {
+                writer.write(pessoa.toString());
+                writer.newLine();
+            }
+            System.out.println("\nLista de pessoas salva em " + arquivo);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 
     public static void main(String[] args){ //Metodo principal
@@ -76,6 +145,3 @@ public class PessoaView  extends JFrame {
 
     }
 }
-
-
-
